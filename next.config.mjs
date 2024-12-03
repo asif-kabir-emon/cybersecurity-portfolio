aca6706995bfd37d090fb014edef73963ac6bcc4
MD ASIF KABIR EMON <asifkabiremon@gmail.com>
2024-12-03 20:30:09 +0800

    Update next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  basePath: isProd ? "/cybersecurity-portfolio" : "",
-  output: "export",
-  distDir: "dist",
   images: {
     remotePatterns: [
       {
