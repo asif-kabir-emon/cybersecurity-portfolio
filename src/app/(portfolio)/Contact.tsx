78ac72b4dc95e5a7c071c83d9c9d92c8103a7574
MD ASIF KABIR EMON <asifkabiremon@gmail.com>
2025-01-07 17:45:48 +0800

    Add contact  section

--- src/app/(portfolio)/Contact.tsx
+++ src/app/(portfolio)/Contact.tsx
@@ -1,35 +1,49 @@
+import { Github, Linkedin, Mail } from "lucide-react";
 import React from "react";
 
 const contactData = [
   {
     title: "Email",
     link: "mailto:asifkabiremon@gmail.com",
+    text: "asifkabiremon@gmail.com",
+    icon: <Mail size={40} />,
   },
   {
     title: "LinkedIn",
-    link: "https://www.linkedin.com/in/asifkabir/",
+    link: "https://www.linkedin.com/in/asif-kabir-emon/",
+    text: "asif-kabir-emon",
+    icon: <Linkedin size={40} />,
   },
   {
     title: "GitHub",
     link: "https://github.com/asif-kabir-emon",
+    text: "asif-kabir-emon",
+    icon: <Github size={40} />,
   },
 ];
 
 const Contact = () => {
   return (
     <div id="contact" className="py-5">
-      <h1 className="text-2xl font-semibold">Contact Me</h1>
-      <div>
-        <div className="space-x-2 mt-4">
-          {Array.from({ length: 30 }).map((_, index) => (
-            <div key={index} className="flex items-center space-x-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-              aliquam modi quae illum, sunt iure ratione, temporibus vitae
-              rerum, tempore earum. Doloremque in animi libero explicabo
-              deleniti reprehenderit laboriosam temporibus.
-            </div>
-          ))}
-        </div>
+      <h1 className="text-2xl font-bold">Contact</h1>
+      <div className="flex flex-col md:flex-row gap-7 items-start mt-5">
+        {contactData.map((data, index) => (
+          <div
+            key={index}
+            className="border-[1.5px] px-5 py-5 inline-block rounded-lg w-full space-y-2 md:w-1/3 text-center"
+          >
+            <span className="flex justify-center">{data.icon}</span>
+            <h2 className="text-lg font-semibold mb-2">{data.title}</h2>
+            <a
+              href={data.link}
+              target="_blank"
+              rel="noreferrer"
+              className="text-md hover:underline"
+            >
+              {data.text}
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
